cf0f5bbbfb46bc29342095e0f5e8d08f05c596a0
Shlomi-Lantser <92504985+Shlomi-Lantser@users.noreply.github.com>
2022-08-11 18:32:53 +0300

    Add files via upload

---
+++ webAppFinal/app/app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask import Blueprint
+import socket
+
+view = Blueprint('view' , __name__)
+
+@view.route('/ip')
+def homePage():
+    hostname = socket.gethostname()
+    IPAddr = socket.gethostbyname(hostname)
+    returning = "<h1>Hello welcome to my web app \n Your Computer IP Address is: <h1>" + IPAddr
+    return returning
+
+
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(view)
+    return app
+
+app = create_app()
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', debug=True)
